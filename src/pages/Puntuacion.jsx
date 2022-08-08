23eac8b3a1e9ddd9a0dcc7070431053ffd74d236
Wida30 <wida30@yahoo.es>
2022-08-08 20:11:55 +0200

    formato contacto

--- src/pages/Puntuacion.jsx
+++ src/pages/Puntuacion.jsx
@@ -5,17 +5,19 @@ const Puntuacion = () => {
   const [estado, setEstado] = useState(0);
 
   useEffect(() => {
-    console.log("Soy el useEffect");
+   
   }, [estado]);
 
   return (
     <section className="puntuacion">
-      <h2 className="tituloPuntuacion">Puntuacion</h2>
+      <h2 className="tituloPuntuacion"> Tu puntuacion:</h2>
 
       <div className="resultado">
         
         <button onClick={() => setEstado(estado + 1)}>Correcta</button>
         <h3>{estado}</h3>
+        <button onClick={() => setEstado(estado - 1)}>Fallo</button>
+        
       </div>
     </section>
   );
