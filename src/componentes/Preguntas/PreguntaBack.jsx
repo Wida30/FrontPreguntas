5058d7747da3892152ce91b483b39c4e9f13be9a
Wida30 <wida30@yahoo.es>
2022-08-08 19:54:53 +0200

    popup

--- src/componentes/Preguntas/PreguntaBack.jsx
+++ src/componentes/Preguntas/PreguntaBack.jsx
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+
 import { SWContext } from "../../context/context";
-
+import Popup from "reactjs-popup";
 import "./PreguntaBack.scss";
-
+import "./PreguntaDetail.scss";
 
 const PreguntaBack = () => {
   const { preguntas } = useContext(SWContext);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 8000);
+  });
 
   return (
     <>
@@ -15,14 +22,33 @@ const PreguntaBack = () => {
 
         {preguntas &&
           preguntas.map((pregunta) => (
-           
-              <Link key={pregunta._id} to={`${pregunta._id}`}>
-              <div className="backCard"></div>
-              </Link>
-           
+            <Popup trigger={<div className="backCard"></div>} position="center">
+              {(close) => (
+                <div className="cartaDetalle">
+                  {pregunta ? (
+                    <>
+                      <h3 className="tituloPregunta">{pregunta.pregunta}</h3>
+
+                      {/* {!visible ? null : (
+                        <p className="tituloRespuesta">{pregunta.respuesta}</p>
+                      )} */}
+
+                      <Popup trigger={<button>Ver respuesta</button>} position="center" >
+                      <p className="tituloRespuesta">{pregunta.respuesta}</p>
+                      </Popup>
+
+                      
+                    </>
+                  ) : null}
+
+                  {/* <a className="close" onClick={close} href="close">
+                    &times;
+                  </a> */}
+                </div>
+              )}
+            </Popup>
           ))}
       </section>
-    
     </>
   );
 };
